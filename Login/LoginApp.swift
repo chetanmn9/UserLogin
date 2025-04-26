b4a52414e72d53172e344733e7908c218108a1b9
Chetan <chetan.mn9@gmail.com>
2025-04-26 10:15:30 +1000

    Initial Commit

---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by Chetan on 26/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoginTests/LoginTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTests.swift
+//  LoginTests
+//
+//  Created by Chetan on 26/4/2025.
+//
+
+import Testing
+@testable import Login
+
+struct LoginTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
